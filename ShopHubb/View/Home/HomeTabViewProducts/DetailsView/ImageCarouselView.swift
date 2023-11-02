80c6b64c1aa8e2399547854718d7e9fc3ba6c843
MJUDDIN01 <mjuddin88@gmail.com>
2023-11-02 12:21:32 +0000

    Added Expendable description, rating and price next to each other

--- ShopHubb/View/Home/HomeTabViewProducts/DetailsView/ImageCarouselView.swift
+++ ShopHubb/View/Home/HomeTabViewProducts/DetailsView/ImageCarouselView.swift
@@ -15,48 +15,48 @@ struct ImageCarouselView: View {
     
     
     var body: some View {
-        HStack {
-            TabView(selection: $currentindext) {
-                ForEach(0..<imageURLs.count, id: \.self) { index in
-                    AsyncImage(url: imageURLs[index]) { image in
-                        image
-                            .resizable()
-                            .scaledToFit()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: imageWidth, height: 200)
-                    } placeholder: {
-                        Text(" ")
-                    }
-                    .tag(index)
-                    .padding(.bottom, 30)
-                    .background(Color.white)
-                }
-            }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-            .animation(.easeInOut, value: currentindext)
-            .onReceive(timer) { _ in
-                withAnimation {
-                    currentindext = (currentindext + 1) % imageURLs.count
-                }
-            }
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack(spacing: 10) {
-                    ForEach(1..<imageURLs.count, id: \.self) { index in
+            HStack {
+                TabView(selection: $currentindext) {
+                    ForEach(0..<imageURLs.count, id: \.self) { index in
                         AsyncImage(url: imageURLs[index]) { image in
                             image
                                 .resizable()
                                 .scaledToFit()
                                 .aspectRatio(contentMode: .fit)
-                                .frame(width: 50, height: 50)
+                                .frame(width: imageWidth, height: 200)
                         } placeholder: {
                             Text(" ")
                         }
+                        .tag(index)
+                        .padding(.bottom, 40)
                     }
                 }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+                .animation(.easeInOut, value: currentindext)
+                .onReceive(timer) { _ in
+                    withAnimation {
+                        currentindext = (currentindext + 1) % imageURLs.count
+                    }
+                }
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack {
+                        ForEach(1..<imageURLs.count, id: \.self) { index in
+                            AsyncImage(url: imageURLs[index]) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 50, height: 50)
+                            } placeholder: {
+                                Text(" ")
+                            }
+                        }
+                        .padding(.trailing, 5)
+                    }
+                }
+               
             }
-           
-        }
        
 
         
--- ShopHubb/View/Home/HomeTabViewProducts/DetailsView/ProductDetails.swift
+++ ShopHubb/View/Home/HomeTabViewProducts/DetailsView/ProductDetails.swift
@@ -3,55 +3,67 @@ import SwiftUI
 
 struct ProductDetails: View {
     let product: Products
+    @State private var isDescriptionExpanded: Bool = false
     var body: some View {
-        HStack {
-            VStack {
-                // Mark:- Load image in full
-                ImageCarouselView(imageURLs: product.imagesUrl)
-//                    .frame(height: 200)
-                Spacer()
-                    HStack {
-                        Text("Price:").bold()
-                        Text("\(Int(discountedPrice()).toCurrency())")
-                            .bold()
-                            .foregroundColor(Color.red)
-                        Text("\(product.price.toCurrency())")
-                            .strikethrough()
-                            .foregroundColor(Color.gray)
-                      
-                    }
-                }
-            }
-        
         VStack(alignment: .leading) {
             HStack {
-                Text("Category:")
-                    .bold()
-                Text(product.category)
+                // Mark:- Load image in full
+                ImageCarouselView(imageURLs: product.imagesUrl)
             }
-            .padding(2)
-            VStack(alignment: .leading) {
-                Text("Description: ")
-                    .bold()
-                Text(product.description)
-            }
-            .padding(2)
-            HStack {
-                Text("Stock:").bold()
-                Text("\(product.stock)")
-            }
-            HStack {
-                Text("Rating:")
-                    .bold()
-                StarRatingView(rating: product.rating)
-            }
-            .padding(2)
             Spacer()
+            HStack {
+                StarRatingView(rating: product.rating)
+                Spacer()
+                Text("\(Int(discountedPrice()).toCurrency())")
+                    .bold()
+                    .foregroundColor(Color.red)
+                Text("\(product.price.toCurrency())")
+                    .strikethrough()
+                    .foregroundColor(Color.gray)
+            }
+            Spacer()
+            HStack {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Description")
+                            .bold()
+                        Image(systemName: isDescriptionExpanded ? "arrow.up.circle" : "arrow.down.circle")
+                            .onTapGesture {
+                                withAnimation {
+                                    isDescriptionExpanded.toggle()
+                                }
+                            }
+                    }
+                    .foregroundColor(Color.blue)
+                    if isDescriptionExpanded {
+                        HStack {
+                            Text("Category:")
+                                .bold()
+                            Text(product.category)
+                        }
+                        .padding(2)
+                        Text(product.description)
+                            .multilineTextAlignment(.leading)
+                        
+                    }
+                    
+                    
+                }
+            }
+            .padding(2)
+            HStack {
+                Text("Hurry up Only")
+                Text("\(product.stock) \(product.title)")
+                    .foregroundColor(Color.red)
+                Text("stocks left!!!")
+            }
+            Spacer()
+                .padding(2)
                 .navigationBarTitle(product.title, displayMode: .inline)
         }
         .padding()
     }
     private func discountedPrice() -> Double {
         return Double(product.price - Int(product.discountPercentage))
-        }
+    }
 }
